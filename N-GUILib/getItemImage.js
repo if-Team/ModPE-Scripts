c504efda63d54ccc554453add2b1fb7da4170403
아포카토맨 <colombia2@naver.com>
2015-02-11 14:08:55 +0900

    Trash to trashcan

--- N-GUILib/getItemImage.js
+++ N-GUILib/getItemImage.js
@@ -6,31 +6,19 @@ var reader = new java.io.BufferedReader(new java.io.InputStreamReader(pectx.getA
 eval("images = "+reader.readLine());
 reader.close();
 var items_opaque = android.graphics.BitmapFactory.decodeStream(pectx.getAssets().open("images/items-opaque.png"));
-var terrain_atlas = pectx.getAssets().open("images/terrain-atlas.tga");
+var width = items_opaque.getWidth();
+var height = items_opaque.getHeight();
 
 function getItemBitmap(data) {
 	var result = null;
 	images.forEach(function(element) {
 		if(element.name == data[0]&&element.uvs[data[1]] != null) {
-			var bgnX = element.uvs[data[1]][0]*256;
-			var bgnY = element.uvs[data[1]][1]*256;
-			var endX = element.uvs[data[1]][2]*256;
-			var endY = element.uvs[data[1]][3]*256;
+			var bgnX = element.uvs[data[1]][0]*width;
+			var bgnY = element.uvs[data[1]][1]*height;
+			var endX = element.uvs[data[1]][2]*width;
+			var endY = element.uvs[data[1]][3]*height;
 			result = android.graphics.Bitmap.createBitmap(items_opaque, bgnX, bgnY, endX-bgnX, endY-bgnY);
 		}
 	});
 	return result;
-}
-
-function getImage(is) {
-	var buf = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, is.available());
-	is.read(buf);
-	var bimg = android.graphics.BitmapFactory.decodeByteArray(buf, 0, buf.length);
-	return bimg;
-}
-
-function useItem() {
-	var b = new GUILib.GUIButton(0,0,128,64,"");
-	b.btn.setImageBitmap(getImage(terrain_atlas));
-	b.render();
 }
