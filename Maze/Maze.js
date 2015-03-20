712fadfaeeecfcfcd308c1ccf110caa35737ddf5
ChalkPE <amato0617@gmail.com>
2015-03-20 22:57:05 +0900

    Updated Maze.js

--- Maze/Maze.js
+++ Maze/Maze.js
@@ -14,65 +14,172 @@
  * limitations under the License.
  */
 
-/**
- * Create a maze
- * @param {number} width
- * @param {number} height
- * @returns {number[][]} The maze
- * @see https://github.com/dstromberg2/maze-generator
- * @license None
- * @todo It is not a perfect maze
- */
-function createMaze(width, height){
-    var totalCells = width * height;
-    var cells = [];
-    var unvis = [];
-    for(var i = 0; i < height; i++){
-        cells[i] = [];
-        unvis[i] = [];
-        for(var j = 0; j < width; j++){
-            cells[i][j] = [0, 0, 0, 0];
-            unvis[i][j] = true;
+// Globals
+var ProcGen = {};
+
+ProcGen.dir = ['up', 'down', 'left', 'right'];
+ProcGen.oDir = { up: 'down', down: 'up', left: 'right', right: 'left' };
+
+ProcGen.rand = function(max) {
+    return Math.floor(max * Math.random());
+};
+
+ProcGen.maze = function(params) {
+    // Initialize grid
+    var grid = {};
+    grid.data = [];
+    grid.params = params;
+
+    var h = grid.params.h;
+    var w = grid.params.w;
+    grid.size = h * w;
+    grid.full = 0;
+    var x;
+    var y;
+    for(x = 0; x < w; x++) {
+        grid.data[x] = [];
+        for(y = 0; y < h; y++) {
+            grid.data[x][y] = { visited: false, up: false, down: false, left: false, right: false };
         }
     }
 
-    var currentCell = [Math.floor(Math.random() * height), Math.floor(Math.random() * width)];
-    var path = [currentCell];
-    unvis[currentCell[0]][currentCell[1]] = false;
-    var visited = 1;
+    // Start at a random position.
+    x = ProcGen.rand(w);
+    y = ProcGen.rand(h);
+    var direction = ProcGen.dir[ProcGen.rand(4)];
 
-    while(visited < totalCells){
-        var pot = [[currentCell[0] - 1, currentCell[1]    , 0, 2],
-                   [currentCell[0]    , currentCell[1] + 1, 1, 3],
-                   [currentCell[0] + 1, currentCell[1]    , 2, 0],
-                   [currentCell[0]    , currentCell[1] - 1, 3, 1]];
-        var neighbors = [];
+    ProcGen._maze(grid, x, y, direction);
 
-        for(var l = 0; l < 4; l++){
-            if(pot[l][0] > -1 && pot[l][0] < height && pot[l][1] > -1 && pot[l][1] < width && unvis[pot[l][0]][pot[l][1]]){
-                neighbors.push(pot[l]);
+    return grid;
+};
+
+ProcGen._maze = function(grid, x, y, direction) {
+    var h = grid.params.h;
+    var w = grid.params.w;
+    var turn = grid.params.turn;
+    var branch = grid.params.branch;
+    var reconnect = grid.params.reconnect;
+    var deadEnd = grid.params.deadEnd;
+
+    grid.full++;
+
+    grid.data[x][y].visited = true;
+    var newDirection = direction;
+
+    if(Math.random() < deadEnd) return;
+
+    // First, figure out what direction we're going.
+    // We randomly decide whether to turn or not.  If not, we continue
+    // on in the direction we're given.  Otherwise, we turn left or right.
+    if(Math.random() < turn) {
+        if(direction == 'up' || direction == 'down') {
+            newDirection = (Math.random() < 0.5) ? 'left' : 'right';
+        } else if(direction == 'left' || direction == 'right') {
+            newDirection = (Math.random() < 0.5) ? 'up' : 'down';
+        }
+    }
+
+    var dx = 0;
+    var dy = 0;
+
+    if(newDirection == 'up') {
+        dy = -1;
+    } else if(newDirection == 'down') {
+        dy = 1;
+    } else if(newDirection == 'left') {
+        dx = -1;
+    } else if(newDirection == 'right') {
+        dx = 1;
+    }
+
+    if(!ProcGen._maze_visited(grid, x + dx, y + dy)) {
+        grid.data[x][y][newDirection] = true;
+        grid.data[x+dx][y+dy][ProcGen.oDir[newDirection]] = true;
+        ProcGen._maze(grid, x + dx, y + dy, newDirection);
+    } else if (Math.random() < reconnect && ProcGen._maze_in_bounds(grid, x + dx, y + dy)) {
+        grid.data[x][y][newDirection] = true;
+        grid.data[x+dx][y+dy][ProcGen.oDir[newDirection]] = true;
+    } else {
+        // If the new direction is blocked, then we should try the two other directions as well.
+        var ds = [];
+        for(var a = 0; a < 4; a++) {
+            if(ProcGen.dir[a] != newDirection && ProcGen.dir[a] != ProcGen.oDir[direction]) {
+                ds.push(ProcGen.dir[a]);
             }
         }
 
-        if(neighbors.length){
-            var next = neighbors[Math.floor(Math.random() * neighbors.length)];
+        if(Math.random() < 0.5) {
+            td = ds[0];
+            ds[0] = ds[1];
+            ds[1] = td;
+        }
 
-            cells[currentCell[0]][currentCell[1]][next[2]] = 1;
-            cells[next[0]][next[1]][next[3]] = 1;
+        for(i = 0; i < 2; i++) {
+            dx = 0;
+            dy = 0;
+            var nd = ds[i];
+            if(nd == 'up') {
+                dy = -1;
+            } else if(nd == 'down') {
+                dy = 1;
+            } else if(nd == 'left') {
+                dx = -1;
+            } else if(nd == 'right') {
+                dx = 1;
+            }
 
-            unvis[next[0]][next[1]] = false;
-            currentCell = [next[0], next[1]];
-            path.push(currentCell);
-            visited++;
-        }else{
-            currentCell = path.pop();
+            if(!ProcGen._maze_visited(grid, x + dx, y + dy)) {
+                grid.data[x][y][nd] = true;
+                grid.data[x+dx][y+dy][ProcGen.oDir[nd]] = true;
+                ProcGen._maze(grid, x + dx, y + dy, nd);
+            }
         }
     }
-    return cells;
-}
+
+    // Now handle branching paths
+    // It's simplest here just to try all four directions.  At least two will always
+    // be blocked, but that's fine.
+    for(var i = 0; i < 4; i++) {
+        if(Math.random() < branch) {
+            dx = 0;
+            dy = 0;
+            var nd = ProcGen.dir[i];
+
+            if(nd == 'up') {
+                dy = -1;
+            } else if(nd == 'down') {
+                dy = 1;
+            } else if(nd == 'left') {
+                dx = -1;
+            } else if(nd == 'right') {
+                dx = 1;
+            }
+
+            if(!ProcGen._maze_visited(grid, x + dx, y + dy)) {
+                grid.data[x][y][nd] = true;
+                grid.data[x+dx][y+dy][ProcGen.oDir[nd]] = true;
+                ProcGen._maze(grid, x + dx, y + dy, nd);
+            }
+        }
+    }
+};
+
+ProcGen._maze_visited = function(grid, x, y) {
+    if(!ProcGen._maze_in_bounds(grid, x, y)) {
+        return true;
+    }
+    return grid.data[x][y].visited;
+};
+
+ProcGen._maze_in_bounds = function(grid, x, y) {
+    return !(x < 0 || x >= grid.params.w || y < 0 || y >= grid.params.h);
+
+};
+
+
 /**
  * Build a maze in the minecraft world!
- * @param {number[][]} maze - The maze
+ * @param {object} grid - The maze
  * @param {number} startX
  * @param {number} startY
  * @param {number} startZ
@@ -80,39 +187,44 @@ function createMaze(width, height){
  * @param {number} [blockDamage = 0]
  * @param {number} [blockHeight = 3] - The height of maze
  */
-function buildMaze(maze, startX, startY, startZ, blockId, blockDamage, blockHeight){
+function buildMaze(grid, startX, startY, startZ, blockId, blockDamage, blockHeight){
     blockId = blockId || 42;
     blockDamage = blockDamage || 0;
     blockHeight = blockHeight || 3;
 
-    for(var i = 0; i < maze.length; i++){
-        for(var j = 0; j < maze[i].length; j++){
-            var cell = maze[i][j];
+    for(var i = 0; i < grid.params.w; i++){
+        for(var j = 0; j < grid.params.h; j++){
+            var cell = grid.data[i][j];
 
             var x = startX + i * 2;
             var z = startZ + j * 2;
+
             for(var k = 0; k < blockHeight; k++){
                 var y = startY + k;
-                if(cell[0] === 0){ //top
+
+                if(!cell.visited){
+                    Level.setTile(x, startY - 1, z, blockId, blockDamage);
+                }else if(!cell.up){
                     Level.setTile(x - 1, y, z - 1, blockId, blockDamage);
                     Level.setTile(x    , y, z - 1, blockId, blockDamage);
                     Level.setTile(x + 1, y, z - 1, blockId, blockDamage);
                 }
-                if(cell[1] === 0){ //right
-                    Level.setTile(x + 1, y, z - 1, blockId, blockDamage);
-                    Level.setTile(x + 1, y, z    , blockId, blockDamage);
-                    Level.setTile(x + 1, y, z + 1, blockId, blockDamage);
-                }
-                if(cell[2] === 0){ //bottom
+                if(!cell.down){
                     Level.setTile(x - 1, y, z + 1, blockId, blockDamage);
                     Level.setTile(x    , y, z + 1, blockId, blockDamage);
                     Level.setTile(x + 1, y, z + 1, blockId, blockDamage);
                 }
-                if(cell[3] === 0){ //left
+                if(!cell.left){
                     Level.setTile(x - 1, y, z - 1, blockId, blockDamage);
                     Level.setTile(x - 1, y, z    , blockId, blockDamage);
                     Level.setTile(x - 1, y, z + 1, blockId, blockDamage);
                 }
+                if(!cell.right){
+                    Level.setTile(x + 1, y, z - 1, blockId, blockDamage);
+                    Level.setTile(x + 1, y, z    , blockId, blockDamage);
+                    Level.setTile(x + 1, y, z + 1, blockId, blockDamage);
+                }
+
             }
         }
     }
@@ -146,7 +258,9 @@ function procCmd(str){
             return;
         }
 
-        var maze = [[]];
+        var width = 0;
+        var height = 0;
+
         var startX = Math.floor(Player.getX());
         var startY = Math.floor(Player.getY()) - 1;
         var startZ = Math.floor(Player.getZ());
@@ -169,11 +283,13 @@ function procCmd(str){
                 return;
 
             case 2:
-                maze = createMaze(params[0], params[1]);
+                width = params[0];
+                height = params[1];
                 break;
 
             case 5:
-                maze = createMaze(params[0], params[1]);
+                width = params[0];
+                height = params[1];
 
                 blockId = params[2];
                 blockDamage = params[3];
@@ -182,7 +298,13 @@ function procCmd(str){
         }
 
         new java.lang.Thread({run: function(){
-            buildMaze(maze, startX, startY, startZ, blockId, blockDamage, blockHeight);
+            buildMaze(ProcGen.maze({
+                w: width, h: height,
+                turn: 0.2,
+                branch: 1,
+                reconnect: 0,
+                deadEnd: 0
+            }), startX, startY, startZ, blockId, blockDamage, blockHeight);
             clientMessage("The maze has been created!")
         }}).start();
     }
