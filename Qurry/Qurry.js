2574b57ae10b74d65df7f7a849b0309f4aeb34fc
ChalkPE <amato0617@gmail.com>
2015-03-31 01:44:25 +0900

    Updated Qurry.js

--- Qurry/Qurry.js
+++ Qurry/Qurry.js
@@ -1,8 +1,11 @@
 function useItem(x, y, z, i, b, s, id, bd) {
 	for(var gy = 127; gy >= 0; gy--) {
-		if(Level.getTile(x, gy, z) != 0 && Level.getTile(x, gy, z) != 7) {
-			Player.addItemInventory(Level.getTile(x, gy, z), 1, Level.getTile(x, gy, z));
-			Level.setTile(x, gy, z, 0)
+        var blockId = Level.getTile(x, gy, z);
+        var blockData = Level.getData(x, gy, z);
+
+		if(blockId !== 0 && blockId !== 7) {
+			Player.addItemInventory(blockId, 1, blockData);
+			Level.destroyBlock(x, gy, z, false);
 		}
 	}
 }
