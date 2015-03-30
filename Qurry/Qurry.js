8c51949c5d804743ee1990ab160b263c072b1d09
programmerDark <angello10@naver.com>
2015-03-31 00:02:18 +0900

    addItemInventory => Player.addItemInventory //깃허브 사용법 배우느라 썼습니다 꾸벅

--- Qurry/Qurry.js
+++ Qurry/Qurry.js
@@ -1,7 +1,7 @@
 function useItem(x, y, z, i, b, s, id, bd) {
 	for(var gy = 127; gy >= 0; gy--) {
 		if(Level.getTile(x, gy, z) != 0 && Level.getTile(x, gy, z) != 7) {
-			addItemInventory(Level.getTile(x, gy, z), 1, Level.getTile(x, gy, z));
+			Player.addItemInventory(Level.getTile(x, gy, z), 1, Level.getTile(x, gy, z));
 			Level.setTile(x, gy, z, 0)
 		}
 	}
