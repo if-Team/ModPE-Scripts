d6afa4caa52e4718d8114d823567da5f280eb03f
CodeInside <scgtdy7151@gmail.com>
2015-04-05 01:59:02 +0900

    update: set Skin method

--- Quarry/Quarry.js
+++ Quarry/Quarry.js
@@ -22,11 +22,64 @@ var Tile = {
  * @since 2015-04-04
  * @author Choseul <chocoslime05@naver.com>
  */
- 
-Block.defineBlock(200, "Qurry", [ ["cauldron_side",0],["cauldron_top",0],["cauldron_bottom",0],["cauldron_side",0], ["cauldron_side",0],["cauldron_side",0]], 0, true, 0);
-Block.defineBlock(201, "Qurry", [ ["cauldron_side",0],["cauldron_top",0],["cauldron_side",0],["cauldron_bottom",0], ["cauldron_side",0],["cauldron_side",0]], 0, true, 0);
-Block.defineBlock(202, "Qurry", [ ["cauldron_side",0],["cauldron_top",0],["cauldron_side",0],["cauldron_side",0], ["cauldron_bottom",0],["cauldron_side",0]], 0, true, 0);
-Block.defineBlock(203, "Qurry", [ ["cauldron_side",0],["cauldron_top",0],["cauldron_side",0],["cauldron_side",0], ["cauldron_side",0],["cauldron_bottom",0]], 0, true, 0);
+
+var QuarryData = [];
+입력방법 : push([mainX, mainY, mainZ, state, startX, startY, startZ, endX, endY, endZ, crainX, crainZ, crainC, drill, drillX, drillY, drillZ]);
+/**
+메인블럭 위치
+QuarryData.X = new Array();
+QuarryData.Y = new Array();
+QuarryData.Z = new Array();
+시작점
+QuarryData.sX = new Array();
+QuarryData.sY = new Array();
+QuarryData.sZ = new Array();
+끝점
+QuarryData.eX = new Array();
+QuarryData.eY = new Array();
+QuarryData.eZ = new Array();
+메인블럭 정보
+QuarryData.fuel = new Array();
+QuarryData.status = new Array();
+QuarryData.slot = new Array();
+수평 X축 크래인 위치
+QuarryData.HX = new Array();
+QuarryData.HXX = new Array();
+QuarryData.HXY = new Array();
+QuarryData.HXZ = new Array();
+수평 X축 크래인 속도
+QuarryData.HXvelX = new Array();
+QuarryData.HXvelY = new Array();
+QuarryData.HXvelZ = new Array();
+수평 Z축 크래인 위치
+QuarryData.HZ = new Array();
+QuarryData.HZX = new Array();
+QuarryData.HZY = new Array();
+QuarryData.HZZ = new Array();
+수평 Z축 크래인 속도
+QuarryData.HZvelX = new Array();
+QuarryData.HZvelY = new Array();
+QuarryData.HZvelZ = new Array();
+수평 크래인 연결부위
+QuarryData.C = new Array();
+QuarryData.CX = new Array();
+QuarryData.CY = new Array();
+QuarryData.CZ = new Array();
+드릴 위치
+QuarryData.D = new Array();
+QuarryData.DX = new Array();
+QuarryData.DY = new Array();
+QuarryData.DZ = new Array();
+드릴 속도
+QuarryData.DvelX = new Array();
+QuarryData.DvelY = new Array();
+QuarryData.DvelZ = new Array();
+*/
+
+Block.defineBlock(200, "Quarry", [ ["cauldron_side",0],["cauldron_top",0],["cauldron_bottom",0],["cauldron_side",0], ["cauldron_side",0],["cauldron_side",0]], 0, true, 0);
+Block.defineBlock(201, "Quarry", [ ["cauldron_side",0],["cauldron_top",0],["cauldron_side",0],["cauldron_bottom",0], ["cauldron_side",0],["cauldron_side",0]], 0, true, 0);
+Block.defineBlock(202, "Quarry", [ ["cauldron_side",0],["cauldron_top",0],["cauldron_side",0],["cauldron_side",0], ["cauldron_bottom",0],["cauldron_side",0]], 0, true, 0);
+Block.defineBlock(203, "Quarry", [ ["cauldron_side",0],["cauldron_top",0],["cauldron_side",0],["cauldron_side",0], ["cauldron_side",0],["cauldron_bottom",0]], 0, true, 0);
 Block.defineBlock(204, "Frame", [ ["cauldron_inner",0],["cauldron_inner",0],["cauldron_inner",0],["cauldron_inner",0], ["cauldron_inner",0],["cauldron_inner",0]], 0, false, 0);
 Block.setRenderLayer(204,3);
 Block.setLightOpacity(204, 1);
@@ -93,3 +146,21 @@ function useItem(x, y, z, itemId, blockId, side, itemDamage, blockDamage){
 		}
 	}
 }
+
+function setSkin(skinFile, skinName){
+	try{
+		var dir = new java.io.File(android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/data/net.zhuoweizhang.mcpelauncher.pro/files/textures/images/mobs/" + skinName + ".png");
+		dir.getParentFile().mkdirs(); 
+		bis = new java.io.BufferedInputStream(new java.io.FileInputStream(skinFile));
+		var bos = new java.io.BufferedOutputStream(new java.io.FileOutputStream(dir));
+		var buffer = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+		var count = 0;
+		while((count = bis.read(buffer)) >= 0){
+			bos.write(buffer, 0, count);
+		}
+		bis.close();
+		bos.close();
+	}catch(e){
+		print(skinName + " 스킨파일이 없습니다");
+	}
+};
