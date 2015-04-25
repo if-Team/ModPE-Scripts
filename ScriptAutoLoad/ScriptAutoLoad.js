9cbf2f155680e4362d3ef4de4b2cb1e06d798c97
CodeInside <scgtdy7151@gmail.com>
2015-04-26 01:21:36 +0900

    update: mainBtn, BtnMove

--- ScriptAutoLoad/ScriptAutoLoad.js
+++ ScriptAutoLoad/ScriptAutoLoad.js
@@ -1,4 +1,4 @@
-var ScriptName = "ScriptAutoLoad";
+var ScriptName = "Script AutoLoad";
 var Version = "dev-v0.1";
 var VersionCode = "100";
 var author = "CodeInside";
@@ -40,7 +40,7 @@ var _MOD_TEST = new java.io.File(_MOD_DIR, "test.txt");
 function _MAP_DIR() {return new java.io.File(_SD_CARD, "games/com.mojang/minecraftWorlds/" + Level.getWorldDir() + "/mods")}
 function _MAP_DATA() {return new java.io.File(_MAP_DIR(), ScriptName + ".data")}
 var DIP = FOUR * loadData(_MOD_DATA, "DIPS");
-if(DIP + "" === "NaN"){
+if(/*DIP + "" === "NaN"*/true){
 	DIP = FOUR;
 }
 
@@ -195,7 +195,7 @@ C,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,D
 
 
 function dp(dips) {
-	return dips*DIP; //parseInt(dips * ctx.getResources().getDisplayMetrics().density + 0.5);
+	return /*dips*DIP;*/parseInt(dips * ctx.getResources().getDisplayMetrics().density + 0.5);
 }
 
 function debug(str) {
@@ -541,5 +541,243 @@ function loadSetting(article) {
 /**
  * ScriptAutoLoad
  */
- 
- 
+SAL = {};
+SAL.mainBtnMod = false;
+
+if(!_MOD_DIR.exists()) {
+	_MOD_DIR.mkdirs();
+}
+
+if(!_MOD_DATA.exists()) {
+	_MOD_DATA.createNewFile();
+}
+
+if(!_FONT.exists()) {
+	if(!downloadFile(_FONT, "https://www.dropbox.com/s/y1o46b2jkbxwl3o/minecraft.ttf?dl=1")) {
+		toast("[" + ScriptName + "]\n\n폰트를 다운로드하지 못했습니다\n아마도 인터넷이 연결되어 있지 않습니다");
+		toasts("[" + ScriptName + "]\n\n시스템 폰트를 적용합니다...");
+	}
+}
+
+SAL.mainBtnLayout = new android.widget.RelativeLayout(ctx);
+SAL.mainBtn = new android.widget.Button(ctx);
+SAL.mainBtn.setText(ScriptName);
+SAL.mainBtn.setTextSize(android.util.TypedValue.COMPLEX_UNIT_PX, DIP*10);
+if(_FONT.exists()) {
+	SAL.mainBtn.setTypeface(android.graphics.Typeface.createFromFile(android.os.Environment.getExternalStorageDirectory() + "/games/com.mojang/minecraftpe/Mods/minecraft.ttf"));
+};
+SAL.mainBtn.setShadowLayer(1/Math.pow(10,10), DIP*10/7, DIP*10/7, android.graphics.Color.DKGRAY);
+SAL.mainBtn.setId(7210);
+SAL.mainBtn_param = new android.widget.RelativeLayout.LayoutParams(DIP*120, DIP*30);
+SAL.mainBtn.setLayoutParams(SAL.mainBtn_param);
+SAL.mainBtn.setBackgroundDrawable(mcpeBtn9);
+SAL.mainBtn.setOnTouchListener(android.view.View.OnTouchListener({
+	onTouch: function(view, event) {
+		try {
+			switch(event.action){
+				case android.view.MotionEvent.ACTION_DOWN:
+					SAL.viewX = event.getX();
+					SAL.viewY = event.getY();
+					view.setBackgroundDrawable(mcpeBtnClick9);
+					view.setTextColor(android.graphics.Color.parseColor("#fbfa9a"));
+					break;
+				case android.view.MotionEvent.ACTION_UP:
+					view.setBackgroundDrawable(mcpeBtn9);
+					view.setTextColor(android.graphics.Color.WHITE);
+					SAL.mainBtnMod = false;
+					break;
+				case android.view.MotionEvent.ACTION_MOVE:
+					SAL.screenX = event.getRawX();
+					SAL.screenY = event.getRawY();
+					SAL.Wx = SAL.screenX - SAL.viewX;
+					SAL.Wy = SAL.screenY - SAL.viewY;
+					if(SAL.mainBtnMod) {
+						uiThread(function() {try {
+							SAL.mainBtnWindow.update(SAL.Wx - (SAL.mainBtnWindow.getWidth() / 2), SAL.Wy - (SAL.mainBtnWindow.getHeight() / 2), SAL.mainBtnWindow.getWidth(), SAL.mainBtnWindow.getHeight(), true);
+						}catch(e) {
+							showError(e);
+						}});
+					}
+					break;
+			}
+		}catch(e) {
+			showError(e);
+		}
+		return false;
+	}
+}));
+
+SAL.mainBtn.setOnClickListener(android.view.View.OnClickListener({
+	onClick: function(view, event) {try {
+		print(ScriptName);
+	}catch(e) {
+		showError(e);
+	}}
+}));
+SAL.mainBtn.setOnLongClickListener(android.view.View.OnLongClickListener({
+	onLongClick: function(view, event) {
+		try {
+			SAL.mainBtnMod = true;
+			toasts("버튼을 이동할 위치로 드래그 하세요");
+		}catch(e) {
+			showError(e);
+		}
+		return true;
+	}
+}));
+SAL.mainBtnLayout.addView(SAL.mainBtn);
+SAL.mainBtnWindow = new android.widget.PopupWindow(SAL.mainBtnLayout, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, false);
+SAL.mainBtnWindow.setSplitTouchEnabled(true);
+SAL.mainBtnWindow.setOutsideTouchable(true);
+//SAL.mainBtnWindow.setTouchable(false);
+
+uiThread(function() {try {
+	SAL.mainBtnWindow.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT|android.view.Gravity.TOP, ((loadData(_MOD_DATA, "WINDOW_X") == null || loadData(_MOD_DATA, "WINDOW_X") == "undefined") ? 0 : loadData(_MOD_DATA, "WINDOW_X") - DIP*16), ((loadData(_MOD_DATA, "WINDOW_Y") == null || loadData(_MOD_DATA, "WINDOW_Y") == "undefined") ? 0 : loadData(_MOD_DATA, "WINDOW_Y") - DIP*30));
+}catch(e) {
+	showError(e);
+}});
+
+/*
+ //PocketGear function
+var Gear = {};
+Gear.mainWindow = null;
+Gear.saveCount = 0;
+Gear.mod = parseInt(loadData(_MOD_DATA, "MOD"));
+if(Gear.mod + "" === "NaN") {
+	Gear.mod = 0;
+	saveData(_MOD_DATA, "MOD", Gear.mod);
+}
+//RECENT, OVERALL, CLOCK, INGAME_CLOCK
+Gear.isRemote = false;
+Gear.allowRemote = (loadData(_MOD_DATA, "ALLOW_REMOTE") == "true");
+Gear.isWindowAlive = false;
+Gear.players = [];
+Gear.chattyPlayers = [];
+Gear.slowTick = 0;
+Gear.slowestTick = 0;
+Gear.noPlayer = 0;
+Gear.voidClip = [0,6,8,9,10,11,26,27,30,31,32,37,38,39,40,50,51,59,63,64,65,66,68,71,78,83,92,95,96,104,105,106,111,126,127,141,142,175,244];
+
+Gear.layout = new android.widget.RelativeLayout(ctx);
+Gear.layout.setBackgroundDrawable(mcpeBGT9);
+Gear.layout.setPadding(DIP*8, DIP*8, DIP*8, DIP*8);
+
+Gear.textView = new android.widget.TextView(ctx);
+Gear.textView.setId(721);
+Gear.textView_param = new android.widget.RelativeLayout.LayoutParams(DIP*80, android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
+Gear.textView.setLayoutParams(Gear.textView_param);
+Gear.textView.setBackgroundDrawable(mcpeTextView9);
+Gear.textView.setPadding(DIP*4, DIP*4, DIP*4, DIP*4);
+Gear.textView.setGravity(android.view.Gravity.RIGHT);
+Gear.textView.setTextSize(android.util.TypedValue.COMPLEX_UNIT_PX, DIP*10);
+Gear.textView.setTextColor(android.graphics.Color.WHITE);
+Gear.textView.getPaint().setAntiAlias(false);
+if(_FONT.exists()) {
+	Gear.textView.setTypeface(android.graphics.Typeface.createFromFile(android.os.Environment.getExternalStorageDirectory() + "/games/com.mojang/minecraftpe/Mods/minecraft.ttf"));
+}
+Gear.textView.setText("Idle mode");
+Gear.layout.addView(Gear.textView);
+
+Gear.moveButton = new android.widget.Button(ctx);
+Gear.moveButton.setId(722);
+Gear.moveButton_param = new android.widget.RelativeLayout.LayoutParams(DIP*56, DIP*20);
+Gear.moveButton_param.setMargins(0,DIP*2,0,0);
+Gear.moveButton_param.addRule(android.widget.RelativeLayout.BELOW, Gear.textView.getId());
+Gear.moveButton.setLayoutParams(Gear.moveButton_param);
+//Gear.moveButton.setAlpha(0);
+Gear.moveButton.setPadding(DIP*4,DIP,0,0);
+Gear.moveButton_drawable = new android.graphics.drawable.GradientDrawable();
+Gear.moveButton_drawable.mutate().setColor(android.graphics.Color.rgb(0x3a, 0x39, 0x3a));
+Gear.moveButton_drawable.setCornerRadius(DIP*5);
+Gear.moveButton.setBackgroundDrawable(Gear.moveButton_drawable);
+Gear.moveButton.setOnTouchListener(new android.view.View.OnTouchListener({ onTouch: function(view, event) {
+	switch(event.action) {
+		case android.view.MotionEvent.ACTION_DOWN:
+				Gear.viewX = event.getX();
+				Gear.viewY = event.getY();
+				break;
+		case android.view.MotionEvent.ACTION_MOVE:
+			Gear.screenX = event.getRawX();
+			Gear.screenY = event.getRawY();
+			Gear.Wx = Gear.screenX - Gear.viewX;
+			Gear.Wy = Gear.screenY - Gear.viewY;
+			uiThread(function() {try {
+				Gear.mainWindow.update(Gear.Wx - DIP*17, Gear.Wy - DIP*30, Gear.mainWindow.getWidth(), Gear.mainWindow.getHeight(), true);
+				debug("move" + Gear.Wx + " " + Gear.Wy);
+			}catch(e) {
+				showError(e);
+			}});
+			break;
+		case android.view.MotionEvent.ACTION_UP:
+			saveData(_MOD_DATA, "WINDOW_X", Gear.Wx);
+			saveData(_MOD_DATA, "WINDOW_Y", Gear.Wy);
+			break;
+	}
+	return true;
+}}));
+Gear.moveButton.setGravity(android.view.Gravity.CENTER);
+Gear.moveButton.setTextSize(android.util.TypedValue.COMPLEX_UNIT_PX, DIP*8);
+Gear.moveButton.setTextColor(android.graphics.Color.WHITE);
+Gear.moveButton.getPaint().setAntiAlias(false);
+if(_FONT.exists()) {
+	Gear.moveButton.setTypeface(android.graphics.Typeface.createFromFile(android.os.Environment.getExternalStorageDirectory() + "/games/com.mojang/minecraftpe/Mods/minecraft.ttf"));}
+Gear.moveButton.setText("Gear");
+Gear.layout.addView(Gear.moveButton);
+
+Gear.resetButton = new android.widget.Button(ctx);
+Gear.resetButton.setId(723);
+Gear.resetButton.setBackgroundDrawable(mcpeMiniBtn9);
+Gear.resetButton_param = new android.widget.RelativeLayout.LayoutParams(DIP*20, DIP*20);
+Gear.resetButton_param.setMargins(DIP*4,DIP*2,0,0);
+Gear.resetButton_param.addRule(android.widget.RelativeLayout.BELOW, Gear.textView.getId());
+Gear.resetButton_param.addRule(android.widget.RelativeLayout.RIGHT_OF, Gear.moveButton.getId());
+Gear.resetButton.setLayoutParams(Gear.resetButton_param);
+Gear.resetButton.setOnTouchListener( new android.view.View.OnTouchListener({ onTouch: 
+	function(view, event){
+		switch(event.action){
+			case android.view.MotionEvent.ACTION_DOWN:
+				//버튼에 손댈때
+				view.setBackgroundDrawable(mcpeMiniBtnClick9);
+				break;
+			case android.view.MotionEvent.ACTION_UP:
+				//버튼에 손땔때
+				view.setBackgroundDrawable(mcpeMiniBtn9);
+				break;
+		}
+		return false;
+	}
+}));
+Gear.resetButton.setOnClickListener(new android.view.View.OnClickListener({onClick: function(view, event) {try {
+		Gear.currentStepLock = Gear.floorStep;
+		uiThread(function() {try {
+			if(Gear.mod !== 0) {
+				Gear.mod = 0;
+				saveData(_MAP_STEP_DATA(), "MOD", Gear.mod);
+			}
+			Gear.textView.setTextColor(android.graphics.Color.WHITE);
+			Gear.textView.setText((Gear.floorStep - Gear.currentStepLock) + "");
+		}catch(e) {
+			showError(e);
+		}});
+		saveData(_MAP_STEP_DATA(), "CURRENT_STEP_LOCK", Gear.currentStepLock);
+		if(net.zhuoweizhang.mcpelauncher.ScriptManager.isRemote && !Gear.isRemote && Gear.allowRemote) {
+		net.zhuoweizhang.mcpelauncher.ScriptManager.handleMessagePacketCallback("", "BlockLauncher, enable scripts, please and thank you");
+		Gear.isRemote = true;
+		newLevel("multi");
+		}
+	}catch(e) {
+		showError(e);
+	}}}));
+Gear.resetButton.setOnLongClickListener(new android.view.View.OnLongClickListener({onLongClick: function(view, event) {try {
+	gearSetting();
+	return true;
+}catch(e) {
+	showError();
+}}}));
+Gear.layout.addView(Gear.resetButton);
+
+Gear.mainWindow = new android.widget.PopupWindow(Gear.layout, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, false);
+Gear.mainWindow.setSplitTouchEnabled(true);
+Gear.mainWindow.setOutsideTouchable(true);
+//Gear.mainWindow.setTouchable(false);
+*/
