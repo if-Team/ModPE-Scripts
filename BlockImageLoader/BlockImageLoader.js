e2c9baba01a01cefe85c01a462ec14330acf0018
affogatoman <colombia2@naver.com>
2015-07-19 21:25:59 +0900

    Added many render-types

--- BlockImageLoader/BlockImageLoader.js
+++ BlockImageLoader/BlockImageLoader.js
@@ -6,6 +6,18 @@
 /**
  * @namespace
  */
+BlockTypes = {
+    CUBE: 0,
+    STAIR: 1,
+    SLAB: 2,
+    SNOW: 3,
+    CARPET: 4,
+    TRAPDOOR: 5
+};
+
+/**
+ * @namespace
+ */
 var BlockImageLoader = {};
 
 BlockImageLoader.TGA = null;
@@ -18,19 +30,19 @@ BlockImageLoader.META_MAPPED = null;
  * @param {Bitmap} tga
  */
 BlockImageLoader.init = function(tga) {
-	if(tga instanceof android.graphics.Bitmap)
-		BlockImageLoader.TGA = tga;
-	
-	if(BlockImageLoader.META == null)
-		BlockImageLoader.META = eval(new java.lang.String(ModPE.getBytesFromTexturePack("images/terrain.meta"))+'');
-		
-	if(BlockImageLoader.META_MAPPED == null)
-		BlockImageLoader.META_MAPPED = BlockImageLoader.META.map(function(e) {
-			return e.name;
-		});
-		
-	if(BlockImageLoader.TGA == null)
-		BlockImageLoader.TGA = net.zhuoweizhang.mcpelauncher.texture.tga.TGALoader.load(ModPE.openInputStreamFromTexturePack("images/terrain-atlas.tga"), false);
+    if(tga instanceof android.graphics.Bitmap)
+        BlockImageLoader.TGA = tga;
+    
+    if(BlockImageLoader.META == null)
+        BlockImageLoader.META = eval(new java.lang.String(ModPE.getBytesFromTexturePack("images/terrain.meta"))+'');
+        
+    if(BlockImageLoader.META_MAPPED == null)
+        BlockImageLoader.META_MAPPED = BlockImageLoader.META.map(function(e) {
+            return e.name;
+        });
+        
+    if(BlockImageLoader.TGA == null)
+        BlockImageLoader.TGA = net.zhuoweizhang.mcpelauncher.texture.tga.TGALoader.load(ModPE.openInputStreamFromTexturePack("images/terrain-atlas.tga"), false);
 };
 
 /**
@@ -39,14 +51,14 @@ BlockImageLoader.init = function(tga) {
  * @returns {Bitmap}
  */
 BlockImageLoader.getBlockBitmap = function(name, data) {
-	var uvs = BlockImageLoader.META[BlockImageLoader.META_MAPPED.indexOf(name)].uvs[data];
-	var x = uvs[0]*BlockImageLoader.TGA.getWidth();
-	var y = uvs[1]*BlockImageLoader.TGA.getHeight();
-	var width = uvs[2]*BlockImageLoader.TGA.getWidth()-x;
-	width = Math.ceil(width);
-	var height = uvs[3]*BlockImageLoader.TGA.getHeight()-y;
-	height = Math.ceil(height);
-	return android.graphics.Bitmap.createBitmap(BlockImageLoader.TGA, x, y, width, height);
+    var uvs = BlockImageLoader.META[BlockImageLoader.META_MAPPED.indexOf(name)].uvs[data];
+    var x = uvs[0]*BlockImageLoader.TGA.getWidth();
+    var y = uvs[1]*BlockImageLoader.TGA.getHeight();
+    var width = uvs[2]*BlockImageLoader.TGA.getWidth()-x;
+    width = Math.ceil(width);
+    var height = uvs[3]*BlockImageLoader.TGA.getHeight()-y;
+    height = Math.ceil(height);
+    return android.graphics.Bitmap.createScaledBitmap(android.graphics.Bitmap.createBitmap(BlockImageLoader.TGA, x, y, width, height), 32, 32, false);
 };
 
 /**
@@ -60,28 +72,48 @@ BlockImageLoader.getBlockBitmap = function(name, data) {
  * @returns {Bitmap}
  */
 BlockImageLoader.create = function(left, right, top, renderType, hasNoShadow) {
-	if(BlockImageLoader.TGA == null || BlockImageLoader.META == null)
-		throw new Error("BlockImageLoader hasn't been initialized");
-	
-	if(!Array.isArray(left) || !Array.isArray(right) || !Array.isArray(top))
-		throw new Error("Illegal argument type");
-	
-	var temp = android.graphics.Bitmap.createBitmap(51, 57, android.graphics.Bitmap.Config.ARGB_8888);
-	left = BlockImageLoader.getBlockBitmap(left[0], left[1]);
-	right = BlockImageLoader.getBlockBitmap(right[0], right[1]);
-	top = BlockImageLoader.getBlockBitmap(top[0], top[1]);
-	
-	switch(renderType) {
-		case 10:
-		temp = BlockImageLoader.createStair(left, right, top, temp, hasNoShadow);
-		break;
-		
-		default:
-		temp = BlockImageLoader.createCube(left, right, top, temp, hasNoShadow);
-		break;
-	}
-	
-	return temp;
+    if(BlockImageLoader.TGA == null || BlockImageLoader.META == null)
+        throw new Error("BlockImageLoader hasn't been initialized");
+    
+    if(!Array.isArray(left) || !Array.isArray(right) || !Array.isArray(top))
+        throw new Error("Illegal argument type");
+    
+    var temp = android.graphics.Bitmap.createBitmap(51, 57, android.graphics.Bitmap.Config.ARGB_8888);
+    left = BlockImageLoader.getBlockBitmap(left[0], left[1]);
+    right = BlockImageLoader.getBlockBitmap(right[0], right[1]);
+    top = BlockImageLoader.getBlockBitmap(top[0], top[1]);
+    
+    switch(renderType) {
+        case BlockTypes.CUBE:
+            temp = BlockImageLoader.createCube(left, right, top, temp, hasNoShadow, 32);
+            break;
+        
+        case BlockTypes.STAIR:
+            temp = BlockImageLoader.createStair(left, right, top, temp, hasNoShadow);
+            break;
+        
+        case BlockTypes.SLAB:
+            temp = BlockImageLoader.createCube(left, right, top, temp, hasNoShadow, 16);
+            break;
+        
+        case BlockTypes.SNOW:
+            temp = BlockImageLoader.createCube(left, right, top, temp, hasNoShadow, 4);
+            break;
+        
+        case BlockTypes.CARPET:
+            temp = BlockImageLoader.createCube(left, right, top, temp, hasNoShadow, 2);
+            break;
+        
+        case BlockTypes.TRAPDOOR:
+            temp = BlockImageLoader.createCube(left, right, top, temp, hasNoShadow, 6);
+            break;
+        
+        default:
+            temp = BlockImageLoader.createCube(left, right, top, temp, hasNoShadow, 32);
+            break;
+    }
+    
+    return temp;
 };
 
 /**
@@ -90,28 +122,30 @@ BlockImageLoader.create = function(left, right, top, renderType, hasNoShadow) {
  * @param {Bitmap} top
  * @param {Bitmap} temp
  * @param {Boolean} hasNoShadow
+ * @param {Number} height
  */
-BlockImageLoader.createCube = function(left, right, top, temp, hasNoShadow) {
+BlockImageLoader.createCube = function(left, right, top, temp, hasNoShadow, height) {
     var createCubeLeft = function(src) {
-        src = android.graphics.Bitmap.createScaledBitmap(src, 25, 32, false);
-        var mSrc = [0, 0, 25, 0, 25, 32, 0, 32];
-        var mDst = [0, 0, 25, 12, 25, 44, 0, 32];
+        src = android.graphics.Bitmap.createBitmap(src, 0, 32-height, 32, height);
+        src = android.graphics.Bitmap.createScaledBitmap(src, 25, height, false);
+        var mSrc = [0, 0, 25, 0, 25, height, 0, height];
+        var mDst = [0, 0, 25, 12, 25, 12+height, 0, height];
         var mtrx = new android.graphics.Matrix();
         mtrx.setPolyToPoly(mSrc, 0, mDst, 0, 4);
         return android.graphics.Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), mtrx, false);
     };
     
     var createCubeRight = function(src) {
-        src = android.graphics.Bitmap.createScaledBitmap(src, 26, 32, false);
-        var mSrc = [0, 0, 26, 0, 26, 32, 0, 32];
-        var mDst = [0, 12, 26, 0, 26, 32, 0, 44]
+        src = android.graphics.Bitmap.createBitmap(src, 0, 32-height, 32, height);
+        src = android.graphics.Bitmap.createScaledBitmap(src, 26, height, false);
+        var mSrc = [0, 0, 26, 0, 26, height, 0, height];
+        var mDst = [0, 12, 26, 0, 26, height, 0, 12+height]
         var mtrx = new android.graphics.Matrix();
         mtrx.setPolyToPoly(mSrc, 0, mDst, 0, 4);
         return android.graphics.Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), mtrx, false);
     };
     
     var createCubeTop = function(src) {
-        src = android.graphics.Bitmap.createScaledBitmap(src, 32, 32, false);
         var mSrc = [0, 0, 32, 0, 32, 32, 0, 32];
         var mDst = [0, 13.5, 25, 0, 51, 13.5, 25, 26];
         var mtrx = new android.graphics.Matrix();
@@ -120,19 +154,19 @@ BlockImageLoader.createCube = function(left, right, top, temp, hasNoShadow) {
     };
     
     left = createCubeLeft(left);
-	right = createCubeRight(right);
-	top = createCubeTop(top);
-	
-	var canv = new android.graphics.Canvas(temp);
-	var p = new android.graphics.Paint();
-	if(hasNoShadow != false)
-		p.setColorFilter(new android.graphics.PorterDuffColorFilter(android.graphics.Color.rgb(255-65, 255-65, 255-65), android.graphics.PorterDuff.Mode.MULTIPLY));
-	canv.drawBitmap(left, 0, temp.getHeight()-left.getHeight(), p);
-	if(hasNoShadow != false)
-		p.setColorFilter(new android.graphics.PorterDuffColorFilter(android.graphics.Color.rgb(255-130, 255-130, 255-130), android.graphics.PorterDuff.Mode.MULTIPLY));
-	canv.drawBitmap(right, 25, temp.getHeight()-right.getHeight(), p);
-	canv.drawBitmap(top, 0, 0, null);
-	return temp;
+    right = createCubeRight(right);
+    top = createCubeTop(top);
+    
+    var canv = new android.graphics.Canvas(temp);
+    var p = new android.graphics.Paint();
+    if(hasNoShadow != false)
+        p.setColorFilter(new android.graphics.PorterDuffColorFilter(android.graphics.Color.rgb(255-65, 255-65, 255-65), android.graphics.PorterDuff.Mode.MULTIPLY));
+    canv.drawBitmap(left, 0, temp.getHeight()-left.getHeight(), p);
+    if(hasNoShadow != false)
+        p.setColorFilter(new android.graphics.PorterDuffColorFilter(android.graphics.Color.rgb(255-130, 255-130, 255-130), android.graphics.PorterDuff.Mode.MULTIPLY));
+    canv.drawBitmap(right, 25, temp.getHeight()-right.getHeight(), p);
+    canv.drawBitmap(top, 0, 32-height, null);
+    return temp;
 };
 
 /**
