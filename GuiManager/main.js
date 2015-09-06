830d0794d5d80cb5fdfd714881524ce7c3616876
affogatoman <colombia2@naver.com>
2015-09-06 21:12:11 +0900

    Making Screen

--- GuiManager/main.js
+++ GuiManager/main.js
@@ -100,6 +100,29 @@ var Device = {
 
 
 
+var Minecraft = {
+    
+    setScreen: function(screen) {
+        if(screen instanceof Screen) {
+            screen.init();
+            
+            var context = Device.getContext();
+            var layout = new android.widget.RelativeLayout(context);
+            
+            for(var index = 0; index < screen.viewList.length; index++) {
+                var element = screen.viewList[index];
+                var converted = ViewConverter["convert"+element.getClassName()];
+                layout.addView(converted);
+            }
+        }
+    }
+    
+};
+
+
+
+
+
 function GuiElement(x, y, width, height) {
     if(typeof x === "number")
         this.x = x;
@@ -122,6 +145,11 @@ function GuiElement(x, y, width, height) {
         this.height = 20;
         
     this.ninePatchLayer = null;
+    this.parentGuiElement = null;
+};
+
+GuiElement.prototype.getClassName = function() {
+    return "GuiElement";
 };
 
 GuiElement.prototype.toString = function() {
@@ -142,6 +170,15 @@ GuiElement.prototype.moveTo = function(x, y) {
         this.y = y >= 0 ? (isNaN(y) ? this.y : y) : this.y;
 };
 
+GuiElement.prototype.getParentGuiElement = function() {
+    return this.parentGuiElement;
+};
+
+GuiElement.prototype.setParentGuiElement = function(parent) {
+    if(parent instanceof GuiElement)
+        this.parentGuiElement = parent;
+};
+
 
 
 
@@ -156,6 +193,10 @@ function Button(x, y, width, height, text) {
 }
 
 ClassHelper.inherit(Button, GuiElement);
+
+Button.prototype.getClassName = function() {
+    return "Button";
+};
 
 Button.prototype.toString = function() {
     return "Button("+this.x+", "+this.y+", "+this.width+", "+this.height+", \""+this.text.replace(/\"/g, "\\\"")+"\")";
@@ -172,6 +213,10 @@ function GuiElementContainer(x, y, width, height) {
 }
 
 ClassHelper.inherit(GuiElementContainer, GuiElement);
+
+GuiElementContainer.prototype.getClassName = function() {
+    return "GuiElementContainer";
+};
 
 GuiElementContainer.prototype.toString = function() {
     return "GuiElementContainer("+this.x+", "+this.y+", "+this.width+", "+this.height+")";
@@ -202,13 +247,22 @@ GuiElementContainer.prototype.getChildrenSize = function() {
 
 
 function Header(x, y, width, height, text) {
-    Button.call(x, y, width, height, text);
+    Button.call(this, x, y, width, height, text);
+    
+    if(width === null)
+        this.width = Device.getWidth();
+    if(height === null)
+        this.height = Device.getHeight();
 }
 
 ClassHelper.inherit(Header, Button);
 
+Header.prototype.getClassName = function() {
+    return "Header";
+};
+
 Header.prototype.toString = function() {
-    return "Header("+this.x+", "+this.y+", "+this.width+", "+this.height+", \""+this.text.replace(/\"/g, "\\\"")+"\")";
+    return "Header("+this.x+", "+this.y+", "+this.width+", "+this.height+", \""+this.text.replace(/\"/g, "\"")+"\")";
 };
 
 
@@ -225,8 +279,23 @@ function Screen() {
 
 ClassHelper.inherit(Screen, GuiElement);
 
+Screen.prototype.getClassName = function() {
+    return "Screen";
+};
+
 Screen.prototype.init = function() {
     return;
+};
+
+Screen.prototype.buttonClicked = function(button) {
+    return;
+};
+
+Screen.prototype.pushElement = function(element) {
+    if(element instanceof GuiElement) {
+        element.setParentGuiElement(this);
+        this.viewList.push(element);
+    }
 };
 
 
@@ -261,3 +330,59 @@ var GuiElementFactory = {
     
 };
 
+
+
+
+
+var ViewConverter = {
+    
+    convertButton: function(button) {
+        if(!button instanceof Button)
+            return null;
+        
+        var context = Device.getContext();
+        var btn = new android.widget.Button(context);
+        btn.setText(button.text);
+        
+        var params = new android.widget.RelativeLayout.LayoutParams(button.width, button.height);
+        params.setMargins(button.x, button.y, 0, 0);
+        btn.setLayoutParams(params);
+        
+        return btn;
+    },
+    
+    convertHeader: function(header) {
+        if(!header instanceof Header)
+            return null;
+            
+        
+    },
+    
+    convertGuiElementContainer: function(container) {
+        if(!container instanceof GuiElementContainer)
+            return null;
+    }
+    
+};
+
+/* Testing */
+function MyScreen() {
+    Screen.call(this);
+    
+    this.hdr = null;
+    this.btn = null;
+}
+
+ClassHelper.inherit(MyScreen, Screen);
+
+MyScreen.prototype.init = function() {
+    this.btn = GuiElementFactory.createButton("text", 0, 0, 10, 10);
+    
+    this.pushElement(this.btn);
+    
+    this.hdr = GuiElementFactory.createHeader("Header");
+    
+    this.pushElement(this.hdr);
+};
+
+Minecraft.setScreen(new MyScreen());
